3f2c9e78564520f4a190ce1b4c311b79d247d349
weng <1906338209@qq.com>
2020-01-09 14:38:01 +0100

    login page

--- src/main/resources/static/network.js
+++ src/main/resources/static/network.js
@@ -21,3 +21,18 @@ AjaxRequests.prototype.getAlLists= function(req,successCallback,failCallback){
 };
 
 
+AjaxRequests.prototype.getAlLists= function(req,successCallback,failCallback){
+    successCallback = typeof successCallback === 'function'?successCallback:new Function();
+    failCallback = typeof failCallback === 'function'?failCallback:new Function();
+
+    $.ajax({
+        url:'http://localhost:8080/login',
+        type:'POST',
+        contentType:'application/json',
+        // dataType:"JSON",
+        success:function(data,state){
+            successCallback(JSON.parse(data) ,state)},
+        error:function(data,state){
+            failCallback(data,state)}
+    })
+};
