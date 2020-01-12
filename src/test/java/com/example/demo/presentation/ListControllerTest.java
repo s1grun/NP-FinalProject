478102cca83f46a2eaa9bbf42b3a35b5e15e355a
Sigrun Arna Sigurdardottir <sigruns@tempo.io>
2020-01-12 21:38:44 +0100

    trying to implement a unit test for newList

--- src/test/java/com/example/demo/presentation/ListControllerTest.java
+++ src/test/java/com/example/demo/presentation/ListControllerTest.java
@@ -3,6 +3,7 @@ package com.example.demo.presentation;
 import com.example.demo.domain.ListDTO;
 import com.example.demo.domain.ListEntity;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +14,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import javax.swing.*;
+
 import static java.util.Collections.singletonList;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -22,7 +26,9 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -65,7 +71,34 @@ class ListControllerTest {
     }
 
     @Test
-    void newList() {
+    void newList() throws Exception {
+        ListsModel listsModel = new ListsModel();
+        listsModel.setOwner("2");
+        listsModel.setListid(2);
+        listsModel.setListname("test2");
+
+        JSONObject req = new JSONObject();
+        req.put("listname", listsModel.getListname());
+
+
+        JSONObject res = new JSONObject();
+        res.put("listname", listsModel.getListname());
+        res.put("status",200);
+
+        given(listController.newList(listsModel)).willReturn( res.toString() );
+
+        MvcResult result= mockMvc.perform(post("/addList")
+                .content("{\"listname\":\"test2\"}")
+                .contentType(APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String str = result.getResponse().getContentAsString();
+        System.out.println(str);
+
+        assertThat(new JSONObject(str).get("listname")).isEqualTo(listsModel.getListname());
+
+
     }
 
     @Test
