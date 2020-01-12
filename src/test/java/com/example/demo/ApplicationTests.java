c7d5ccb53c46400c4bffd81990501e0e445ca246
weng <1906338209@qq.com>
2020-01-12 18:57:13 +0100

    test

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.presentation.ListController;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@WebAppConfiguration
 class DemoApplicationTests {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Before
+    public void setup() {
 
-    @Test
-    void contextLoads() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new ListController()).build();
+
+//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
